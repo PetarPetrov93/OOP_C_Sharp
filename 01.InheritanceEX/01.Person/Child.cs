af411a9ea5abcb74cbf7dd9b272bb3eed24cf60c
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-26 13:04:16 +0300

    solutions

---
+++ 01.InheritanceEX/01.Person/Child.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+        public override int Age 
+        {
+            get { return base.Age; }
+
+            set { if (value > 0 && value <= 15) base.Age = value; }
+        }
+    }
+}
---
+++ 01.InheritanceEX/01.Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+       
+		private int age;
+		public Person(string name, int age)
+        {
+            Name = name;
+			Age = age;
+        }
+        public string Name { get; set; }
+		
+		public virtual int Age
+		{
+			get { return age; }
+			set 
+			{
+				if (value > 0)
+				{
+                    age = value;
+                }
+				 
+			}
+		}
+        public override string ToString() => new StringBuilder().Append(String.Format($"Name: {Name}, Age: {Age}")).ToString();
+        
+    }
+}
