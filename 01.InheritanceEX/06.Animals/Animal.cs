3ac10d6792ae82c397b9900161fe859e266613a9
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-27 10:47:40 +0300

    solution

---
+++ 01.InheritanceEX/06.Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+
+        }
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+        
+        public string Gender { get; set; }
+        
+
+        public virtual string ProduceSound() { return string.Empty; }
+    }
+}
---
+++ 01.InheritanceEX/06.Animals/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            string input;
+            while ((input = Console.ReadLine()) != "Beast!")
+            {
+                string[] animalDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = animalDetails[0];
+                int age = int.Parse(animalDetails[1]);
+                string gender = animalDetails[2];
+                if (age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                
+                if (input == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+                    animals.Add(dog);
+                }
+                else if (input == "Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+                    animals.Add(cat);
+                }
+                else if (input == "Kitten")
+                {
+                    Kitten kitten = new Kitten(name, age);
+                    animals.Add(kitten);
+                }
+                else if (input == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(name, age);
+                    animals.Add(tomcat);
+                }
+                else if (input == "Frog")
+                {
+                    Frog frog = new Frog(name, age, gender);
+                    animals.Add(frog);
+                }
+            }
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}
