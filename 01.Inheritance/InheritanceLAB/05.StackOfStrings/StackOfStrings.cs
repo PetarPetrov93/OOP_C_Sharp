7d409e9c257d6c41a0680b2a1a245f8364199f2a
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-27 14:09:42 +0300

    solution

---
+++ 01.Inheritance/InheritanceLAB/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count() == 0 ? true : false;
+
+        public void AddRange(Stack<string> stack) 
+        {
+            foreach (var item in stack)
+            {
+                Push(item);
+            }
+        }
+        
+    }
+}
---
+++ 01.Inheritance/InheritanceLAB/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings strings = new StackOfStrings();
+
+            Console.WriteLine(strings.IsEmpty());
+
+            strings.Push("asd");
+            strings.Push("dfg");
+            strings.Push("xcv");
+
+            Console.WriteLine(strings.IsEmpty());
+
+            Stack<string> defaultStack = new Stack<string>();
+            defaultStack.Push("aaaaa");
+            defaultStack.Push("bbbbb");
+            defaultStack.Push("ccccc");
+
+            strings.AddRange(defaultStack);
+
+            Console.WriteLine(strings.IsEmpty());
+            
+        }
+    }
+}
